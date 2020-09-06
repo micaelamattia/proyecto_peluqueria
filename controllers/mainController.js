c488859f83dd8c51df7a681fe84b43764a9b68d6
micaelamattia <66717388+micaelamattia@users.noreply.github.com>
2020-09-06 01:36:36 -0300

    cambios

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,9 +2,16 @@ const dbProducts = require("../data/dataBase");
 
 module.exports = {
     index:function(req, res, next) {
+        let visitada= dbProducts.filter(producto=>{
+            return producto.category == 'visited'
+        })
+        let ofertas= dbProducts.filter(producto =>{
+            return producto.category == 'in-sale'
+        })
         res.render('index', 
         { title: 'Peluqueria',
-        producto: dbProducts
-    })
+        ofertas: ofertas,
+        visitada:visitada,
+    });
     }
 }
